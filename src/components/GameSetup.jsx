3b6408c07614f64af0795a232de77772b8d97fbe
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2024-08-23 23:37:01 -0600

    Player Select is working when useState in single file but not when trying to send to GameSetup component

--- src/components/GameSetup.jsx
+++ src/components/GameSetup.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import PlayerSelect from "./PlayerSelect.jsx";
-
-function GameSetup() {
-    const [selectedPlayer, setSelectedPlayer] = useState(1); // Initializes the state
-    console.log('setSelectedPlayer function:', setSelectedPlayer);
-
-    return (
-        <div>
-            <PlayerSelect selectedPlayer={selectedPlayer} setSelectedPlayer={setSelectedPlayer}/>
-        </div>
-    );
-}
-
-export default GameSetup;
+// import React, { useState } from 'react';
+// import PlayerSelect from "./PlayerSelect.jsx";
+//
+// // Add a log to confirm the PlayerSelect component is being imported correctly.
+// console.log("Using PlayerSelect from: ", PlayerSelect);
+//
+// function GameSetup() {
+//     const [selectedPlayer, setSelectedPlayer] = useState(setSelectedPlayer[0].id)
+//     // Log to confirm the current state and function
+//     console.log("GameSetup: selectedPlayer state:", selectedPlayer);
+//     console.log("GameSetup: setSelectedPlayer function:", setSelectedPlayer);
+//
+//     return (
+//         <div>
+//             {/* Pass the state and setState function as props */}
+//             <PlayerSelect selectedPlayer={selectedPlayer} setSelectedPlayer={setSelectedPlayer} />
+//         </div>
+//     );
+// }
+//
+// export default GameSetup;
