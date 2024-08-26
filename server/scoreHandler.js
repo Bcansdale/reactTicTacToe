0f365039b735f57f0041d9ea9c795eb204bfc8f2
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2024-08-25 19:16:56 -0600

    api set up and able to pull SCORE_DATA in postman

--- server/scoreHandler.js
+++ server/scoreHandler.js
@@ -1,3 +1,23 @@
 const SCORE_DATA = [
-    {id: 'X'}
-]
+    {id: 'X', score: 0},
+    {id: 'O', score: 0},
+    {id: 'tie', score: 0}
+];
+
+
+const ScoreHandler = {
+    getScores : (req, res) => {
+        res.send({
+            message : 'Score of game',
+            score: SCORE_DATA
+        })
+    },
+
+    addScores : (req, res) => {
+
+    }
+
+
+}
+
+export default ScoreHandler;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import morgan from 'morgan'
 import ViteExpress from 'vite-express'
-// import handlerFunctions from './scoreHandler.js'
-
+import scoreHandler from './scoreHandler.js'
 
 const app = express();
-
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
 app.use(express.json())
+
+
+app.get('/api/scores', scoreHandler.getScores)
 
 
 ViteExpress.config({ printViteDevServerHost: true });
